7a1110ee08ea80ebe6c46d2fde46dd196a256243
dz298 <dz298@cornell.edu>
2021-03-01 21:25:49 -0500

    rename

---
+++ sailboat_sam_e70.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=sailboat_sam_e70.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/sailboat_sam_e70.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=sailboatsame70.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/sailboatsame70.x.tar
