58930605c6c7f48e3ece40d710560e68a4d71862
nicholaskgeorge <nkg37@cornell.edu>
2021-06-29 20:22:58 -0400

    Adding main boatp program

---
+++ SailBoatProgramGit.X/build/sam_e70_xult/production/_ext/1360937237/app_gps.o.d
@@ -0,0 +1,18 @@
+build/sam_e70_xult/production/_ext/1360937237/app_gps.o: ../src/app_gps.c \
+ ../src/app_gps.h ../src/config/sam_e70_xult/configuration.h \
+ ../src/config/sam_e70_xult/user.h \
+ ../src/config/sam_e70_xult/toolchain_specifics.h \
+ ../src/packs/CMSIS/CMSIS/Core/Include/cmsis_compiler.h \
+ ../src/packs/CMSIS/CMSIS/Core/Include/cmsis_gcc.h
+
+../src/app_gps.h:
+
+../src/config/sam_e70_xult/configuration.h:
+
+../src/config/sam_e70_xult/user.h:
+
+../src/config/sam_e70_xult/toolchain_specifics.h:
+
+../src/packs/CMSIS/CMSIS/Core/Include/cmsis_compiler.h:
+
+../src/packs/CMSIS/CMSIS/Core/Include/cmsis_gcc.h:
---
+++ SailBoatProgramGit.X/build/sam_e70_xult/production/_ext/984150034/sys_dma.o.d
@@ -0,0 +1,8 @@
+build/sam_e70_xult/production/_ext/984150034/sys_dma.o: \
+ ../src/config/sam_e70_xult/system/dma/sys_dma.c \
+ ../src/config/sam_e70_xult/system/dma/sys_dma.h \
+ ../src/config/sam_e70_xult/system/dma/sys_dma_mapping.h
+
+../src/config/sam_e70_xult/system/dma/sys_dma.h:
+
+../src/config/sam_e70_xult/system/dma/sys_dma_mapping.h:
